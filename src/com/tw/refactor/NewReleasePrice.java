02b21f7e6be0dac43987aa0c61082bb309cee35c
Sachin Tiwari <tiwarisachin147@gmail.com>
2022-02-19 22:26:21 +0530

    first commit

---
+++ src/com/tw/refactor/NewReleasePrice.java
@@ -0,0 +1,16 @@
+package com.tw.refactor;
+
+public class NewReleasePrice extends Price {
+
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+
+	double getCharge(int daysRented) {
+		return daysRented * 3;
+	}
+
+	int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2 : 1;
+	}
+}
---
+++ src/com/tw/refactor/Price.java
@@ -0,0 +1,12 @@
+package com.tw.refactor;
+
+public abstract class Price {
+	abstract int getPriceCode();
+
+	abstract double getCharge(int daysRented);
+
+	int getFrequentRenterPoints(int daysRented) {
+		return 1;
+	}
+
+}
